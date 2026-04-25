7f441906a5112b89980aa766c7e3c2ba656458cd
Samuel Martoni <s.martoni@studenti.unibg.it>
2026-04-25 15:09:53 +0200

    Aggiunto le pagine HTML
    Pagine contratto-telefonico, header, footer e index aggiunte

---
+++ HTML/contratto-telefonico.php
@@ -0,0 +1,20 @@
+<?php include('header.php') ?>
+
+<div class="sidebar">
+    <h2>Filtro Di Ricerca</h2>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+</div>
+
+<main>
+
+</main>
+
+<?php include('footer.php') ?>
---
+++ HTML/index.php
@@ -0,0 +1,8 @@
+<?php include('header.php'); ?>
+
+<main>
+    <h2>Dashboard</h2>
+    <p>Benvenuti nel sistema di gestione ...</p>
+</main>
+
+<?php include('footer.php'); ?>
