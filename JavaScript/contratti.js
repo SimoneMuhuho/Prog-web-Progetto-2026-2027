9fb1900be393442483e6368e7f39bca93fdbd14e
lcacciarru <167301596+lcacciarru@users.noreply.github.com>
2026-05-04 15:33:55 +0200

    Modifica al path dell'API nel file js dei contratti
    Non veniva più caricato il database nella pagina dei contratti. Problema risolto sistemando il path dell'API togliendo i riferimenti alle cartelle lasciando solamente il nome del file.

--- JavaScript/contratti.js
+++ JavaScript/contratti.js
@@ -4,7 +4,7 @@
  * Gestisce: caricamento tabella, filtri multipli, modal dettaglio
  */
 $(function () {
-    const API = '../PHP/contratti/api_contratti.php'
+    const API = 'api_contratti.php'
 
     /* ══════════════════════════════════════════════════════════════════════
        UTILITY
